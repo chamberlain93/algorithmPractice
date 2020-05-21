09d403b70be2a6eef939ef2e0b2d3b7b2101040e
chamberlain93 <1045709301@qq.com>
2020-05-21 22:32:27 +0800

    first commit

---
+++ src/main/java/com/chamberlain/test/Test.java
@@ -0,0 +1,11 @@
+package com.chamberlain.test;
+
+/**
+ * Created by chamberlain on 2020/5/21.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("hello world!S");
+    }
+}
