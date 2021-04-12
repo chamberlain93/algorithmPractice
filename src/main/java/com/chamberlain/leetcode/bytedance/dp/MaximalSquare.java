8266b957c989a879badd7b4da2d23871411eb398
qiang.yin <qiang.yin@envision-digital.com>
2021-04-12 20:21:29 +0800

    最大正方形

---
+++ src/main/java/com/chamberlain/leetcode/bytedance/dp/MaximalSquare.java
@@ -0,0 +1,11 @@
+package com.chamberlain.leetcode.bytedance.dp;
+
+/**
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ */
+public class MaximalSquare {
+
+    public int maximalSquare(char[][] matrix) {
+        return 1;
+    }
+}
