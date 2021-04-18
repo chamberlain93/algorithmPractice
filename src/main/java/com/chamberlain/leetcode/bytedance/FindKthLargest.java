892b07ffea8a1e147cfdc4235e84d3e83d5ca83f
chamberlain93 <1045709301@qq.com>
2021-04-18 14:33:14 +0800

    数组中第k大的数字

--- src/main/java/com/chamberlain/leetcode/bytedance/FindKthLargest.java
+++ src/main/java/com/chamberlain/leetcode/bytedance/FindKthLargest.java
@@ -9,10 +9,10 @@ public class FindKthLargest {
         int result = 0;
         int length = nums.length;
         build_max_heap(nums, length);
-        while(k > 0){
+        while (k > 0) {
             result = Heap_extract_max(nums, length--);
             k--;
-            if (k == 0){
+            if (k == 0) {
                 break;
             }
         }
@@ -20,47 +20,47 @@ public class FindKthLargest {
         return result;
     }
 
-    void build_max_heap(int heapArray[],int length){//通过普通数组构建最大堆
-        for(int i=length/2;i>=1;i--){
-            Max_Heapify(heapArray,i,length);
+    void build_max_heap(int heapArray[], int length) {//通过普通数组构建最大堆
+        for (int i = length / 2; i >= 1; i--) {
+            Max_Heapify(heapArray, i, length);
         }
     }
 
-    int Heap_extract_max(int heapArray[],int length){//在最大堆里提取key值最大的元素,删除后保持最大堆状态
-        if(length<1){
+    int Heap_extract_max(int heapArray[], int length) {//在最大堆里提取key值最大的元素,删除后保持最大堆状态
+        if (length < 1) {
             return 0;
         }
-        int max=heapArray[0];
-        heapArray[0]=heapArray[length-1];
-        length=length-1;
-        Max_Heapify(heapArray,1,length);//为了让新堆保持最大堆
+        int max = heapArray[0];
+        heapArray[0] = heapArray[length - 1];
+        length = length - 1;
+        Max_Heapify(heapArray, 1, length);//为了让新堆保持最大堆
         return max;
 
     }
 
-    void Max_Heapify(int heapArray[],int i,int length){//调整数组的顺序，使其满足最大堆性质
-        int left=2*i;//左子树跟节点下标
-        int right=2*i+1;//右子树跟节点下标
+    void Max_Heapify(int heapArray[], int i, int length) {//调整数组的顺序，使其满足最大堆性质
+        int left = 2 * i;//左子树跟节点下标
+        int right = 2 * i + 1;//右子树跟节点下标
         int largest;
-        if(left<=length&&heapArray[left-1]>heapArray[i-1]){//左子树根节点大于跟节点
-            largest=left;
-        }else{
-            largest=i;
+        if (left <= length && heapArray[left - 1] > heapArray[i - 1]) {//左子树根节点大于跟节点
+            largest = left;
+        } else {
+            largest = i;
         }
-        if(right<=length&&heapArray[right-1]>heapArray[largest-1]){//左子树根节点大于跟节点
-            largest=right;
+        if (right <= length && heapArray[right - 1] > heapArray[largest - 1]) {//左子树根节点大于跟节点
+            largest = right;
         }
-        if(largest!=i){
-            int temp=heapArray[largest-1];
-            heapArray[largest-1]=heapArray[i-1];
-            heapArray[i-1]=temp;
-            Max_Heapify(heapArray,largest,length);
+        if (largest != i) {
+            int temp = heapArray[largest - 1];
+            heapArray[largest - 1] = heapArray[i - 1];
+            heapArray[i - 1] = temp;
+            Max_Heapify(heapArray, largest, length);
         }
     }
 
     public static void main(String[] args) {
         FindKthLargest findKthLargest = new FindKthLargest();
-        int[] nums = {3,2,3,1,2,4,5,5,6};
+        int[] nums = {3, 2, 3, 1, 2, 4, 5, 5, 6};
         int k = 4;
         System.out.println(findKthLargest.findKthLargest(nums, k));
     }
