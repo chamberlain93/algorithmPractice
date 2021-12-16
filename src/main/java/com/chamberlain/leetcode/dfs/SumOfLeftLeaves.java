2daf61bd7ac9c1a2398852b8fea7d2451bdfda12
qiang.yin <qiang.yin@envision-digital.com>
2021-12-16 20:39:51 +0800

    左叶子结点的和

---
+++ src/main/java/com/chamberlain/leetcode/dfs/SumOfLeftLeaves.java
@@ -0,0 +1,27 @@
+package com.chamberlain.leetcode.dfs;
+
+import com.chamberlain.leetcode.basicClass.TreeNode;
+
+/**
+ * 计算给定二叉树的所有左叶子之和。
+ * https://leetcode-cn.com/problems/sum-of-left-leaves/
+ */
+public class SumOfLeftLeaves {
+
+    int sum = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+
+       sumOfLeftLeaves(root.left);
+        sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
