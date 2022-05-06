f012b4474d0ff8850f3a98ae85b5b9d07aeed21d
qiang.yin <qiang.yin@envision-digital.com>
2022-05-06 17:16:40 +0800

    最近请求数

---
+++ src/main/java/com/chamberlain/leetcode/everydayPractices/RecentCounter.java
@@ -0,0 +1,44 @@
+package com.chamberlain.leetcode.everydayPractices;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * 写一个RecentCounter类来计算特定时间范围内最近的请求。
+ *
+ * 请你实现 RecentCounter 类：
+ *
+ * RecentCounter() 初始化计数器，请求数为 0 。
+ * int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+ * 保证 每次对 ping 的调用都使用比之前更大的 t 值。
+ *
+ * 链接：https://leetcode-cn.com/problems/number-of-recent-calls
+ */
+public class RecentCounter {
+
+    private Queue<Integer> queue = new ArrayDeque<>();
+
+    public RecentCounter() {
+
+    }
+
+    public int ping(int t) {
+        if (queue.size() == 0){
+            queue.add(t);
+            return 1;
+        }
+        while (!queue.isEmpty() && (t - queue.peek()) > 3000){
+            queue.poll();
+        }
+        queue.add(t);
+        return queue.size();
+    }
+
+    public static void main(String[] args) {
+        RecentCounter recentCounter = new RecentCounter();
+        System.out.println(recentCounter.ping(1));     // requests = [1]，范围是 [-2999,1]，返回 1
+        System.out.println(recentCounter.ping(100));   // requests = [1, 100]，范围是 [-2900,100]，返回 2
+        System.out.println(recentCounter.ping(3001));  // requests = [1, 100, 3001]，范围是 [1,3001]，返回 3
+        System.out.println(recentCounter.ping(3002));
+    }
+}
